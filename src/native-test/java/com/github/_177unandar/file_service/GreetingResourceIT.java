f1d7d4c8990028d8eccfcd1e88823583d73cbd43
Achmad Munandar <177unandar@gmail.com>
2024-12-31 03:26:43 +0700

    added: Quarkus Framework

---
+++ src/native-test/java/com/github/_177unandar/file_service/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.github._177unandar.file_service;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
